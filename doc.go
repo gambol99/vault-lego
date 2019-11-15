cec22c3b64a0a2165b49a6f3b34ae1ff6fa23b74
catac <Catalin.Cirstoiu@gmail.com>
2019-11-15 11:09:33 +0100

    readd GITHUB_TOKEN variable; fix GIT_SHA; prepare next release

--- doc.go
+++ doc.go
@@ -22,7 +22,7 @@ import (
 )
 
 var (
-	release = "v0.0.10"
+	release = "v0.0.11"
 	gitsha  = "no gitsha provided"
 	author  = "Rohith Jayawardene"
 	version = release + " (git+sha: " + gitsha + ")"
